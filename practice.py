4ecdf248eaee63e17330efaa4268376e564eb764
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:26:14 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,22 @@
-print('hello')
-print('world')
-print('hello
+# __str__ Method
+
+#inside class Time:
+  
+  def __str__(self):
+      return '%.2d:%.2d:%.2d' % (self.hour, self.minute, self.second)
+    
+ 
+time = Time(9, 45)
+print(time)
+09:45:00
+    
+#operator overloading
+    
+def __add__(self, other)
+    seconds = self.time_to_init() + other.time_to_int()
+    return int_to_time(seconds)
+  
+start = Time(9,45)
+duration = Time(1, 35)
+print(start + duration)
+11:20:00
